2f94f4f164bc48de7239b5139b55a3f55e452b19
losdev <loschdev@gmail.com>
2014-08-28 11:24:53 +0200

    refactoring home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,13 @@ class StaticPagesController < ApplicationController
   end
 
   def home
+    matches = Match.all
+    matches.each do |match|
+      if DateTime.now < match.match_time
+        @matchday = match.matchday
+        break
+      end
+    end
+    @matches = Match.where(matchday: @matchday)
   end
 end
